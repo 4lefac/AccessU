87639ef44e1d74bca87ebbb76d81c5a1a4d247d7
Forlefac Fontem <fontem.2@osu.edu>
2019-05-17 23:06:22 -0400

    added method headers

--- app/api/Routes.js
+++ app/api/Routes.js
@@ -8,39 +8,39 @@ module.exports.Routes = {
   /*
   ** GET request for the main map screen.
   */
- GET_map: () => {
-  return fetch(baseURL + '/locations')
-    .then( (response) => response.json())
-    .then(function(data) {
-      return data;
-    })
-    .catch( (error) => {
-      console.error(error);
-    });
+  GET_map: () => {
+    return fetch(baseURL + '/locations')
+      .then((response) => response.json())
+      .then(function (data) {
+        return data;
+      })
+      .catch((error) => {
+        console.error(error);
+      });
 
   },
   //getting a specific id and return a json object of that entrance
   GET_map_with_id: (entranceID) => {
     return fetch(baseURL + entranceID)
       .then((response) => response.json())
-      .then(function(data) {
+      .then(function (data) {
         return data
       })
       .catch((error) => {
         console.error(error);
       });
-    },
+  },
 
-    //add an entrance data to the database. data has to be a json object or string doens't matter
+  //add an entrance data to the database. data has to be a json object or string doens't matter
   POST_Add: (data) => {
     fetch(baseURL, {
       method: 'POST',
       body: JSON.stringify(data), // data can be `string` or {object}!
-      headers:{
+      headers: {
         'Content-Type': 'application/json'
       }
     }).then(res => res.json())
-    .then(response => alert(JSON.stringify(response)))
-    .catch(error => console.error('Error:', error));
+      .then(response => alert(JSON.stringify(response)))
+      .catch(error => console.error('Error:', error));
   }
 }
---
+++ app/api/User.js
@@ -0,0 +1,53 @@
+
+var baseURL = 'https://accessu-c0933.firebaseapp.com/api/v2';
+
+module.exports.User = {
+    /*
+    ** GET full userInfo Json
+    */
+    GET_userInfo: () => {
+
+    },
+    /*
+    ** GET user basic Profile info
+    */
+    GET_Profile: () => {
+
+    },
+    /*
+    ** GET all comments for the user
+    */
+    GET_all_Comments: () => {
+
+    },
+    /*
+    ** GET all comments for an entrance
+    */
+    GET_all_entrance_comments: (entranceID) => {
+
+    },
+    /*
+    ** GET all comments for a location
+    */
+    GET_all_location_comments: (locationID) => {
+
+    },
+    /*
+    ** GET all added items of a user (added locations and added entrances)
+    */
+    GET_all_added_items: () => {
+
+    },
+    /*
+    ** Post anadded comments by user to an entrance
+    */
+    POST_added_comment_entrance: (entranceID, comment, userID) => {
+
+    },
+    /*
+    ** Post an added comment by user to a location
+    */
+    POST_added_comment_location: (locationID, comment, userID) => {
+
+    }
+}
