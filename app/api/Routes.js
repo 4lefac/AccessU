987d0bef3ff74f4af2c7287ca6f5fd1a703dae10
Forlefac Fontem <fontem.2@osu.edu>
2019-05-31 12:12:49 -0400

    bug fixes for entrance and location add screen

--- app/api/Routes.js
+++ app/api/Routes.js
@@ -1,39 +1,38 @@
 var baseURL = 'https://accessu-c0933.firebaseapp.com/api/v2';
 
 module.exports.Routes = {
-
   /*
-** GET request for the main map screen.
-*/
+   ** GET request for the main map screen.
+   */
   GET_map: () => {
     return fetch(baseURL + '/locations')
-      .then((response) => response.json())
-      .then(function (data) {
+      .then(response => response.json())
+      .then(function(data) {
         return data;
       })
-      .catch((error) => {
+      .catch(error => {
         console.error(error);
       });
   },
 
   /*
-** getting a specific id and return a json object of that entrance
-*/
-  GET_map_with_id: (entranceID) => {
+   ** getting a specific id and return a json object of that entrance
+   */
+  GET_map_with_id: entranceID => {
     return fetch(baseURL + entranceID)
-      .then((response) => response.json())
-      .then(function (data) {
-        return data
+      .then(response => response.json())
+      .then(function(data) {
+        return data;
       })
-      .catch((error) => {
+      .catch(error => {
         console.error(error);
       });
   },
 
   /*
-** add an location data to the database.
-*/
-  POST_Add_Location: (data) => {
+   ** add an location data to the database.
+   */
+  POST_Add_Location: data => {
     return fetch(baseURL + '/location', {
       method: 'POST',
       body: JSON.stringify(data), // data can be `string` or {object}!
@@ -43,15 +42,15 @@ module.exports.Routes = {
     })
       .then(res => res.json())
       .then(response => {
-        return response
+        return response;
       })
       .catch(error => alert(JSON.stringify(error)));
   },
 
   /*
-** add an entrance data to the database.
-*/
-  POST_Add_Entrance: (data) => {
+   ** add an entrance data to the database.
+   */
+  POST_Add_Entrance: data => {
     return fetch(baseURL + '/entrance', {
       method: 'POST',
       body: JSON.stringify(data), // data can be `string` or {object}!
@@ -61,43 +60,40 @@ module.exports.Routes = {
     })
       .then(res => res.json())
       .then(response => {
-        return response
+        return response;
       })
       .catch(error => alert(JSON.stringify(error) + 'error here'));
   },
 
   /*
-** add an image for a location to the database
-*/
+   ** add an image for a location to the database
+   */
   POST_Add_Location_Image: (data, locationID) => {
-    alert(JSON.stringify(data))
     fetch(baseURL + '/images/location/' + locationID, {
       method: 'POST',
       body: data
     })
       .then(res => res.json())
       .then(response => {
-        alert(JSON.stringify(response) + ' location added successfully')
+        return response;
       })
       .catch(error => {
-        alert(JSON.stringify(error) + " error adding location")
+        alert(JSON.stringify(error) + ' error adding location');
       });
   },
 
   /*
-** add an image for a entrance to the database
-*/
+   ** add an image for a entrance to the database
+   */
   POST_Add_Entrance_Image: (data, entranceID) => {
-    alert(JSON.stringify(data))
     fetch(baseURL + '/images/entrance/' + entranceID, {
       method: 'POST',
       body: data
     })
       .then(res => res.json())
       .then(response => {
-        alert(JSON.stringify(response) + ' image added successfully')
+        return response;
       })
       .catch(error => alert(JSON.stringify(error)));
   }
-
-}
+};
